abc91c57079fcdeda05a03904cb031b1f61ec1bd
rohan <korlapuacharyrohan@gmail.com>
2026-03-13 23:00:06 +0530

    rohan

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+//  Primitive
+
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail; // undefined 
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId)
+
+// const bigNumber = 234984644893245
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+ 
+// const heros = ["rohan","ironman"] // arrays
+
+// let obj = {  //Objects
+//     name: "rohan",
+//     age:22,
+// }
+
+// const myFunction = function(){ //Function
+//     console.log("hello rohan")
+// }
+
+// console.log(typeof obj)
