95d19cc42aa2216df09f75dd519b2c40477e4cce
rohan <korlapuacharyrohan@gmail.com>
2026-03-17 12:29:09 +0530

    datesinjs

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
+let myCreatedDate = new Date(2026, 3, 17)
+// console.log(myCreatedDate.toDateString());
+let newDate = new Date()
+// 
+
+
